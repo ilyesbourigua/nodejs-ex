af7236dbd279b0a96d812c8b2667a69f1b165633
ilyesbourigua <ilyesbouriga1@gmail.com>
2020-03-26 10:29:42 +0100

    change server

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express()
 app.get('/',function (req, res) {
   res.send('hello world');
 });
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000 ;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1' ;
 app.listen(port, ip);
